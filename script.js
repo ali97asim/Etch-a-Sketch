f96da1ad73bba95fb96c94f19476ba4c5bd5ecaf
ali97asim <ali.1997asim@gmail.com>
2025-08-19 11:14:34 +0500

    Fix the errors

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
 const container=document.querySelector(".container");
+let gridSize=16;
+const button=document.querySelector("button");
 
-for(let i=0;i<16*16;i++){
-    const div=document.createElement("div")
+const setGrid=()=>{
+    container.textContent=""
+    for(let i=0;i<16*16;i++){
+    const div=document.createElement("div");
     div.classList.add("box");
-
-    container.appendChild(div)
+    div.style.flex=`0 0 calc(100% / ${gridSize})`;
+    div.style.height=`calc(100% / ${gridSize})`;
+    container.appendChild(div);
+    }
 }
-const boxes=document.querySelectorAll(".box")
-boxes.forEach(box=>{
-    box.addEventListener("mouseover",(e)=>{
-    box.classList.add("color")
+setGrid();
+
+button.addEventListener("click",(e)=>{
+    while(true){
+        let ans=parseInt(prompt("Input Grid Size:"))
+        if(ans>100) {
+            alert("Cant Enter size more than 100.")
+        }else{
+            gridSize=ans;
+            setGrid()
+            break;
+        }
+    }
 })
+
+container.addEventListener("mouseover",(e)=>{
+    if(e.target.classList.contains("box")){
+        e.target.classList.add("hovered")
+    }
 })
-document.body.appendChild(container);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
